fc299618d15a0a6f59e5e055af6d52e455a384b5
noor1711 <noornimrat00@gmail.com>
2026-01-29 20:25:59 +0530

    feat: writing to file

--- StripePractise/setup.py
+++ StripePractise/setup.py
@@ -22,3 +22,8 @@ first_sub_price = stripe.Price.create(
 
 pprint(f"Product - {first_sub}")
 pprint(f"Price - {first_sub_price}")
+
+# lets save this to a file as well
+
+with open("product_pricing.txt", "w") as file:
+    file.writelines(f"{first_sub.id} : {first_sub_price.id}")
